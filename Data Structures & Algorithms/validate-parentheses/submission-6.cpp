9e34c88dd300377a56d111f8c83c16b3dfc8df0a
j9m.dev <j9m.dev@gmail.com>
2026-09-21 08:55:27 +0900

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        // q; Open brackets are closed in the correct order. like c
+        // meaning latest open bracket should be closed first LIFO
+        // possible solution
+        // use stack; e.g.) stack./ or vector; can also use string itself as stack
+        
+        stack<char> st;
+        array<char,256> brace_map = {};
+        brace_map[')'] = '(';
+        brace_map['}'] = '{';
+        brace_map[']'] = '[';
+
+        // cannot use in stack
+        // st.reserve(s.size());
+
+        for(auto c: s) {
+            if ( c == '(' || 
+                 c == '{' || 
+                 c == '[')
+            {
+                st.push(c);
+            } else {
+                if ( c == ')' || c == '}' || c == ']') {
+                    if (st.size()==0) return false;
+                    if ( st.top() != brace_map[c] ) return false;
+                    else st.pop();
+                }
+            }
+        }
+        if ( st.size() == 0 ) return true;
+        else return false;
+    }
+};
