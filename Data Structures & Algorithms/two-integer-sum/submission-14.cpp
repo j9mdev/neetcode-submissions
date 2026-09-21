999b56027f48d183131010a1240d12ce292b9afa
j9m.dev <j9m.dev@gmail.com>
2026-09-22 08:14:49 +0900

    Add: two-integer-sum - submission-14

---
+++ Data Structures & Algorithms/two-integer-sum/submission-14.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // brute force
+        for (int i=0; i<(int)nums.size(); i++) {
+            for (int j=i+1; j<(int)nums.size(); j++) {
+                if ( nums[i] + nums[j] == target ) {
+                    return {i,j};
+                }
+            }
+        }
+        // non recheable in theory
+        // return {-1,-1};
+        // didn't know i can return {}, then return {}
+        return {};
+    }
+};
